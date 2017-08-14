29cc0e945035f61c86d90e157e7d3b93d4b367a2
Anna Bush <annabush092@gmail.com>
2017-08-14 20:59:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+  if number>1
+    test_range = (2...number).to_a
+  else
+    return false
+  end
+  test_range.each do |i|
+    if number%i == 0
+      return false
+    end
+  end
+  return true
+end
